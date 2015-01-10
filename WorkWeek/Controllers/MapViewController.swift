7ebef734eed49516713df2b45837afd6a6aec5a7
John Regner <john@johnregner.com>
2015-01-10 14:23:21 -0600

    Added MapViewController

---
+++ WorkWeek/Controllers/MapViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+  @IBOutlet weak var mapView: MKMapView!
+  @IBOutlet weak var searchBar: UISearchBar!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    }
+}
+
+extension MapViewController: UISearchBarDelegate {
+
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        println("Search Button was clicked: \(searchBar.text)")
+        searchBar.resignFirstResponder()
+    }
+}
