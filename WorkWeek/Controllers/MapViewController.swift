c4333bd990139cbf3bcaa4d14fbbacbf2b429b87
John Regner <john@johnregner.com>
2015-01-10 17:34:39 -0600

    basic location updates and beginnings of geofence

--- WorkWeek/Controllers/MapViewController.swift
+++ WorkWeek/Controllers/MapViewController.swift
@@ -21,14 +21,31 @@ class MapViewController: UIViewController {
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     }
+
+    // MARK: - My Geofence
+    
+    //locationmanager.startMonitoringRegion()
+  @IBAction func setWorkLocation(sender: AnyObject) {
+    let managerCenter = locationManager.location.coordinate
+    let workRegion = CLCircularRegion(center: managerCenter, radius: 150.0, identifier: "WorkRegion")
+    println("Setting workRegion: \(workRegion)")
+    locationManager.startMonitoringForRegion(workRegion)
+    //add a thing to the map to show the new region
+    let region: MKCoordinateRegion = MKCoordinateRegion(center: workRegion.center, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    mapView.setRegion(region, animated: true)
+    
+  }
+    
 }
 
-// MARK: - UISearchBarExtension
+extension MapViewController: MKMapViewDelegate {
 
-extension MapViewController: UISearchBarDelegate {
-
-    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
-        println("Search Button was clicked: \(searchBar.text)")
-        searchBar.resignFirstResponder()
+    func mapView(mapView: MKMapView!, didUpdateUserLocation userLocation: MKUserLocation!) {
+        //zoom the map to the users location
+        let coords = userLocation.location.coordinate
+        let region = MKCoordinateRegion(center: coords, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        mapView.setRegion(region, animated: true)
     }
+    
+    
 }
