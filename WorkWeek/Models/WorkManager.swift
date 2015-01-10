67e1d373a22d5038e2fe28618d73b183d0d65f76
John Regner <john@johnregner.com>
2015-01-10 17:30:12 -0600

    Added workManager to handle my model data

---
+++ WorkWeek/Models/WorkManager.swift
@@ -0,0 +1,19 @@
+//
+//  WorkManager.swift
+//  WorkWeek
+//
+//  Created by John Regner on 1/10/15.
+//  Copyright (c) 2015 TRexTech. All rights reserved.
+//
+
+import UIKit
+
+class WorkManager: NSObject {
+   
+    func allItems() -> [String] {
+        let item1 = "M 8:04 - 5:02      8.45Hours"
+        return [item1]
+    }
+    
+    
+}
