3cdd8ef90492d359383c8c6157664da0288df46b
John Regner <john@johnregner.com>
2015-03-06 10:56:41 -0600

    Clean up unused tests. Remove commented code.

--- UnitTests/Tests/WorkManagerTest.swift
+++ UnitTests/Tests/WorkManagerTest.swift
@@ -5,14 +5,6 @@ import WorkWeek
 
 class WorkManagerTest: XCTestCase {
 
-    override func setUp() {
-        super.setUp()
-    }
-
-    override func tearDown() {
-        super.tearDown()
-    }
-
     func testWorkManagerCreation(){
         let manager = WorkManager()
         XCTAssertNotNil(manager, "Manager is not nil")
@@ -57,6 +49,7 @@ class WorkManagerTest: XCTestCase {
     }
 
     func testAllItems(){
+        //TODO: Why is this not implemented?
         //calling all items shoud return an array of work hours objects ready 
         //for placement into a cell in the tableview
 
@@ -124,64 +117,3 @@ class WorkManagerTest: XCTestCase {
         XCTAssert(manager.hoursWorkedThisWeek == 16.1, "Worked 16 Hours and 6 minutes")
     }
 }
-
-//    func testGetMondayEvents(){
-//        let manager = WorkManager()
-//        //since we know that Jan 1, 2001 was a monday. We will use that as our reference date.
-//        // See tests for NSDateExtension to see why we are setting an explicit time zone.
-//        NSTimeZone.setDefaultTimeZone(NSTimeZone(forSecondsFromGMT: 0))
-//
-//        var events = [
-//            NSDate(timeIntervalSinceReferenceDate: 60*60*(24*0+8)),  //8am Jan 1, 2001
-//            NSDate(timeIntervalSinceReferenceDate: 60*60*(24*0+16)), //4pm Jan 1, 2001
-//            NSDate(timeIntervalSinceReferenceDate: 60*60*(24*1+8)),  //8am Jan 2, 2001
-//            NSDate(timeIntervalSinceReferenceDate: 60*60*(24*1+16)), //4pm Jan 2, 2001
-//            NSDate(timeIntervalSinceReferenceDate: 60*60*(24*2+8)),  //8am Jan 3, 2001
-//            NSDate(timeIntervalSinceReferenceDate: 60*60*(24*2+16)), //4pm Jan 3, 2001
-//            NSDate(timeIntervalSinceReferenceDate: 60*60*(24*3+8)),  //8am Jan 4, 2001
-//            NSDate(timeIntervalSinceReferenceDate: 60*60*(24*3+16)), //4pm Jan 4, 2001
-//            NSDate(timeIntervalSinceReferenceDate: 60*60*(24*4+8)),  //8am Jan 5, 2001
-//            NSDate(timeIntervalSinceReferenceDate: 60*60*(24*4+16)), //4pm Jan 5, 2001
-//        ]
-//
-//        manager.addArrival(  events[0])
-//        manager.addDeparture(events[1])
-//        manager.addArrival(  events[2])
-//        manager.addDeparture(events[3])
-//        manager.addArrival(  events[4])
-//        manager.addDeparture(events[5])
-//        manager.addArrival(  events[6])
-//        manager.addDeparture(events[7])
-//        manager.addArrival(  events[8])
-//        manager.addDeparture(events[9])
-//        let monday = manager.getMondayEvents()
-//        let mondayFakeEvents =
-//            [Event(inOrOut: .Arrival,   date: events[0]),
-//             Event(inOrOut: .Departure, date: events[1])]
-////        XCTAssertEqual(mondayFakeEvents, monday, "Correctly Fetched Mondays Events")
-//        XCTAssertEqual(mondayFakeEvents[0], monday[0], "Correctly fetched Mondays Events")
-//        XCTAssertEqual(mondayFakeEvents[1], monday[1], "Correctly fetched Mondays Events")
-//    }
-//    func testGetFirstPair(){
-//
-//        let manager = WorkManager()
-//        //since we know that Jan 1, 2001 was a monday. We will use that as our reference date.
-//        // See tests for NSDateExtension to see why we are setting an explicit time zone.
-//        NSTimeZone.setDefaultTimeZone(NSTimeZone(forSecondsFromGMT: 0))
-//
-//        var events = [
-//            NSDate(timeIntervalSinceReferenceDate: 60*60*(24*0+8)),  //8am Jan 1, 2001
-//            NSDate(timeIntervalSinceReferenceDate: 60*60*(24*0+16)), //4pm Jan 1, 2001
-//            NSDate(timeIntervalSinceReferenceDate: 60*60*(24*1+16)), //4pm Jan 2, 2001
-//            NSDate(timeIntervalSinceReferenceDate: 60*60*(24*2+8)),  //8am Jan 3, 2001
-//        ]
-//
-//        manager.addArrival(  events[0])
-//        manager.addDeparture(events[1])
-//        manager.addArrival(  events[2])
-//        manager.addDeparture(events[3])
-//
-//        let firstPair = manager.getFirstPair()
-//
-//    }
-//
