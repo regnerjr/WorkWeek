2f5c057f7bba5d8fad4ebf63db446784b0850451
John Regner <john@johnregner.com>
2015-03-12 21:14:41 -0500

    Added guard count so that he map View will only zoom in once.
    refactored out the mapRect to fit 2 coordinates function.

--- WorkWeek/Controllers/MapViewController.swift
+++ WorkWeek/Controllers/MapViewController.swift
@@ -47,6 +47,7 @@ class MapViewController: UIViewController {
     }
 
     override func viewDidLoad() {
+        Counter.count = 0 //reset the counter so we can zoom in on the user once, per page load
         super.viewDidLoad()
 
         //draw the current work location if it is not nil
@@ -114,32 +115,29 @@ class MapViewController: UIViewController {
 extension MapViewController: MKMapViewDelegate {
 
     func mapView(mapView: MKMapView!, didUpdateUserLocation userLocation: MKUserLocation!) {
-        println("Updated User Location")
-        let userCoordinates = userLocation.location.coordinate
+        //only do this map zooming thing once
+        if Counter.count == 0 {
+            println("Updated User Location \(Counter.count++)")
+            let userCoordinates = userLocation.location.coordinate
+            //stoping location updates
+            locationManager.stopUpdatingLocation()
 
-        if let overlays = mapView.overlays {
-            //zoom the map so it shows the user and the overlays
+            if let overlays = mapView.overlays {
 
-            if let overlay = overlays.first as? MKCircle {
-                let a = MKMapPointForCoordinate(userCoordinates)
-                let b = MKMapPointForCoordinate(overlay.coordinate)
-                let maprect = MKMapRectMake(min(a.x, b.x), min(a.y,b.y), abs(a.x - b.x), abs(a.y-b.y))
-                mapView.setVisibleMapRect(maprect, edgePadding: UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20),animated: true)
+                //zoom the map so it shows the user and the overlays
+                if let overlay = overlays.first as? MKCircle {
+                    let mapRect = mapRectToFitCoordinate(userCoordinates, andCoordinate: overlay.coordinate)
+                    mapView.setVisibleMapRect(mapRect, edgePadding: UIEdgeInsets(top: 40, left: 40, bottom: 40, right: 40), animated: true)
+                }
+
+            } else {
+                //zoom the map to the users location
+                //not sure how far away from work the person is, so give them a good zoom 2km
+                let region = MKCoordinateRegionMakeWithDistance(userCoordinates, 2000, 2000)
+                mapView.setRegion(region, animated: true)
             }
-
-        } else {
-            //zoom the map to the users location
-            //not sure how far away from work the person is, so give them a good zoom 2km
-            let region = MKCoordinateRegionMakeWithDistance(userCoordinates, 2000, 2000)
-            mapView.setRegion(region, animated: true)
-
         }
-
-        //once we have the users location on the map, stop looking or location updates
-        locationManager.stopUpdatingLocation() //better for user battery
-        println("Done Updating Location")
     }
-
 
     func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
         //only one overlay so dont bother to check it just return a renderer
@@ -159,6 +157,12 @@ extension MapViewController: MKMapViewDelegate {
         addOverLayAtCoordinate(view.annotation.coordinate)
         startMonitoringOneRegionAtCoordinate(view.annotation.coordinate)
         addArrivalIfAtWork()
+    }
+
+    func mapRectToFitCoordinate(one: CLLocationCoordinate2D, andCoordinate two: CLLocationCoordinate2D) -> MKMapRect {
+        let a = MKMapPointForCoordinate(one)
+        let b = MKMapPointForCoordinate(two)
+        return MKMapRectMake(min(a.x, b.x), min(a.y,b.y), abs(a.x - b.x), abs(a.y-b.y))
     }
 
 
