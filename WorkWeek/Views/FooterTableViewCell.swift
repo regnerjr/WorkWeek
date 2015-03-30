d94c77c333c8eec8a6c8297fc5deb2e124762f80
John Regner <john@johnregner.com>
2015-03-30 15:03:12 -0500

    Updated to use custom footer view.

---
+++ WorkWeek/Views/FooterTableViewCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class FooterTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var AtWorkLabel: UILabel!
+    @IBOutlet weak var ArrivedTimeLabel: UILabel!
+    @IBOutlet weak var timeSoFarLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+}
