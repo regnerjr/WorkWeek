5bf834761dd3d16b20d47553e5403e88ed2c053b
John Regner <john@johnregner.com>
2015-05-11 13:32:51 -0500

    Added LocationManager and tests.

---
+++ UnitTests/Tests/LocationManagerTests.swift
@@ -0,0 +1,26 @@
+import UIKit
+import XCTest
+
+import WorkWeek
+
+class LocationManagerTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {
+        XCTAssert(true, "Pass")
+    }
+//
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measureBlock() {
+//        }
+//    }
+
+}
