689256b754c9b72351bbaea137fab0b6cf969e0c
John Regner <regnerjr@gmail.com>
2016-03-23 14:13:48 -0700

    Don't print this path.

--- WorkWeek/Extensions/NSKeyedUnarchiverExtensions.swift
+++ WorkWeek/Extensions/NSKeyedUnarchiverExtensions.swift
@@ -2,7 +2,6 @@ import Foundation
 
 extension NSKeyedUnarchiver {
     static func unarchiveMutableArrayWithFile(path: String?) -> NSMutableArray? {
-        print(path)
         if let path = path {
             return NSKeyedUnarchiver.unarchiveObjectWithFile(path) as? NSMutableArray
         } else {
